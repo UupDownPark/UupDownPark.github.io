13c29fba269c761311a766c4e6a32f6a51ed5fc4
UupDownPark <geakura123@gmail.com>
2024-07-01 14:31:20 +0900

    [feat] ARCHIVING end

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import DeployStack from './assets/images/deploy.jpg'
 import CommunicationDevtools from './assets/images/communicationDevtools.jpg'
 import Certificate  from "./assets/images/qnet.jpg";
 import VersionControl from "./assets/images/versionControl.jpg"
+import Github from './assets/images/githublongwidth.png'
+
 function App() {
   return (
     <div className="flex flex-col justify-center items-center" >
@@ -26,7 +28,7 @@ function App() {
         </div>
       </div>
       <div id="aboutMeContainer" className="flex flex-col justify-center items-center m-10 max-w-6xl">
-        <span className="font-tenada text-[50px] mb-8">ABOUT ME</span>
+        <span className="font-tenada text-[50px] mb-8 border-b-4 border-black">ABOUT ME</span>
         <div className="flex flex-wrap justify-between">
           <div className="mb-8 w-[33%]">   
             <div className="flex flex-row flex-nowrap my-0 mx-auto  max-w-56  w-[100%]">
@@ -98,39 +100,58 @@ function App() {
         
       </div>
       <div className="bg-green-500 w-full flex flex-col justify-center items-center">
-        <span className="font-tenada text-[50px] mb-8 mt-16">SKILLS</span>
-        <div id="skillsContainer" className="max-w-6xl py-16 px-8 content-around flex flex-col flex-wrap h-screen">
+        <span className="font-tenada text-[50px] mb-8 mt-16 border-b-4 border-black">SKILLS</span>
+        <div id="skillsContainer" className="max-w-7xl py-16 content-around flex flex-col flex-wrap h-screen">
     
-              <div id="stackContainerFrontend" className=" w-64 h-fit bg-white rounded-lg shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-12 p-6">
+              <div id="stackContainerFrontend" className="w-72 h-fit bg-white rounded-2xl shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-3 p-6">
                 <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Frontend</div>
                 <img src={FrontEndStack}  />
               </div>
-              <div id="stackContainerBackend" className="w-64 h-fit bg-white rounded-lg shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-12 p-6">
+              <div id="stackContainerBackend" className="w-72 h-fit bg-white rounded-2xl shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-3 p-6">
                 <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Backend</div>
                 <img src={BackEndStack}  />
               </div>
-              <div  className="w-64 h-fit  bg-white rounded-lg shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-12 p-6">
-                <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Communication & Dev tools</div>
-                <img src={CommunicationDevtools}  />
-              </div>
-              <div  className="w-64 h-fit  bg-white rounded-lg shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-12 p-6">
+              <div  className="w-72 h-fit  bg-white rounded-2xl shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-3 p-6">
                 <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Deploy</div>
                 <img src={DeployStack}  />
               </div>
-              <div  className="w-64 h-fit  bg-white rounded-lg shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-12 p-6">
-                <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Version Control</div>
-                <img src={VersionControl}  />
+              <div  className="w-72 h-fit  bg-white rounded-2xl shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-3 p-6">
+                <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Communication <br /> & Dev tools</div>
+                <img src={CommunicationDevtools}  />
               </div>
-            
-              <div  className="w-64 h-fit bg-white rounded-lg shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-12 p-6">
+              <div  className="w-72 h-fit bg-white rounded-2xl shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-3 p-6">
                 <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Certificate</div>
                 <img src={Certificate}  />
                 <span className="font-bold font-tenada">정보처리기사(필기+실기)</span>
               </div>
+              <div  className="w-72 h-fit  bg-white rounded-2xl shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-3 p-6">
+                <div className="font-noto-sans text-2xl text-green-800 pb-2 mb-6 border-b border-slate-200">Version Control</div>
+                <img src={VersionControl}  />
+              </div>
+            
+             
          
         </div>
       </div>
-     
+      <div className="bg-yellow-500 w-full flex flex-col justify-center items-center">
+        <span className="font-tenada text-[50px] mb-8 mt-16 border-b-4 border-black">ARCHIVING</span>
+        <div id="skillsContainer" className="max-w-6xl py-16 px-8 content-around flex flex-col flex-wrap h-2/4">
+              <div id="stackContainerFrontend" className="h-fit bg-white rounded-2xl shadow-2xl transition ease-in-out delay-150 hover:-translate-y-2 mb-8 mx-12 p-12">
+                <img src={Github}  />
+                <a href="https://github.com/UupDownPark?tab=repositories">
+                <div className="font-noto-sans text-lg mb-4">https://github.com/UupDownPark?tab=repositories</div>
+                <div>
+                  <p className="font-noto-sans text-lg mb-4"> <b>소스 코드 저장소</b> 입니다.</p>
+                  <li className="font-noto-sans text-lg"> 교육프로그램에서 진행한 프로젝트의 소스코드 </li>
+                  <li className="font-noto-sans text-lg"> 교육프로그램에서 진행한 클론코딩의 소스코드</li>
+                  <li className="font-noto-sans text-lg"> 코딩 연습을 위해 실습해본 소스 코드 </li>
+                </div>
+                </a>
+              </div>
+             
+         
+        </div>
+      </div>
         
       </div>
   
